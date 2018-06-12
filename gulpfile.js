a275e8630ec563bb3c5c40185f5328256252a42a
dmcclelland <dmcclelland@ptc.com>
2018-06-12 15:17:19 -0400

    add nunjucks implementation

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,29 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync');
+// gulpfile.js
+var gulp              = require('gulp');
+var browserSync       = require('browser-sync');
+var nunjucksRender 		= require('gulp-nunjucks-render'); // importing the plugin
 
 gulp.task('serve', function() {
+    console.log('running server');
     browserSync({
         server: {
-            baseDir: 'app' // Change this to your web root dir
+            baseDir: 'app'
         }
     });
 });
 
-// Default task to be run with `gulp`
+// writing up the gulp nunjucks task
+gulp.task('nunjucks', function() {
+  console.log('nunjucking');
+
+  // configuring the templates folder for nunjucks
+  nunjucksRender.nunjucks.configure(['app/templates/']);
+
+  // get the pages files
+  return gulp.src('app/pages/**/*.+(html|njk)')
+    .pipe(nunjucksRender())
+    .pipe(gulp.dest('app'))
+});
+
+//default task to be run with gulp
 gulp.task('default', ['serve']);
